07c529cc9c00e3bc98080d2d3704c0067bb80004
kotakiran.443@gmail.com <kota@443>
2016-10-29 16:24:14 +0800

    CRUD Operations For Item And Customer

--- src/main/java/com/btcc/wsm/repository/CustomerRepository.java
+++ src/main/java/com/btcc/wsm/repository/CustomerRepository.java
@@ -2,11 +2,14 @@ package com.btcc.wsm.repository;
 
 import com.btcc.wsm.model.Customer;
 
+import com.btcc.wsm.model.Item;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 /**
  * Created by siva on 1/10/2016.
  */
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
-
+    List<Customer> findAllByIsDeletedOrderByCreationTimeDesc(boolean isDeleted);
 }
--- src/main/java/com/btcc/wsm/web/managedbean/CustomerManagedBean.java
+++ src/main/java/com/btcc/wsm/web/managedbean/CustomerManagedBean.java
@@ -1,19 +1,28 @@
 package com.btcc.wsm.web.managedbean;
 
 import com.btcc.wsm.model.Customer;
+import com.btcc.wsm.model.Item;
 import com.btcc.wsm.model.SystemAuditTrail;
+import com.btcc.wsm.model.Users;
 import com.btcc.wsm.service.CustomerService;
 import com.btcc.wsm.service.SystemAuditTrailRecordService;
 import com.btcc.wsm.serviceimpl.CustomerServiceImpl;
+import com.btcc.wsm.util.FacesUtil;
 import com.btcc.wsm.web.datamodel.CustomerDataModel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.faces.application.ConfigurableNavigationHandler;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -24,7 +33,7 @@ import java.util.List;
 @ManagedBean
 @Component
 @ViewScoped
-public class CustomerManagedBean implements MainManagedBean {
+public class CustomerManagedBean implements Serializable {
 
     final static Logger logger = LogManager.getLogger(CustomerManagedBean.class);
 
@@ -33,24 +42,24 @@ public class CustomerManagedBean implements MainManagedBean {
 
     @Autowired
     private SystemAuditTrailRecordService auditTrailRecordService;
-
-    private Customer customer = new Customer();
+    private Users loggedInUser;
+    private Customer newCustomer = new Customer();
     private List<Customer> customerList;
     private Customer selectedCustomer = new Customer();
     private CustomerDataModel customerDataModel;
 
-    public Customer getCustomer() {
-        return customer;
+    public Customer getNewCustomer() {
+        return newCustomer;
     }
 
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
+    public void setNewCustomer(Customer customer) {
+        this.newCustomer = customer;
     }
 
     public List<Customer> getCustomerList() {
-        if(customerList == null){
-            customerList = customerService.findAll();
-        }
+        //if(customerList == null){
+        customerList = customerService.findAll();
+        //}
         return customerList;
     }
 
@@ -73,16 +82,69 @@ public class CustomerManagedBean implements MainManagedBean {
     public void setCustomerDataModel(CustomerDataModel customerDataModel) {
         this.customerDataModel = customerDataModel;
     }
+    public Users getLoggedInUser() {
+        return loggedInUser;
+    }
 
-    public void create() {
+    public void setLoggedInUser(Users loggedInUser) {
+        this.loggedInUser = loggedInUser;
+    }
+    public String create() {
+        Users loggedInUser = (Users) FacesUtil
+                .getSessionMapValue("LOGGEDIN_USER");
+        newCustomer.setCreatedBy(loggedInUser.getUsername());
+        Customer customer=customerService.create(newCustomer);
+        if(customer!=null){
+            newCustomer=new Customer();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("SUCCESS : New Customer Created Successfully"));
 
+        }else{
+            newCustomer=new Customer();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("ERROR : Customer Not Created Successfully"));
+        }
+        return "listcustomer";
     }
 
     public void update() {
-
+        Users loggedInUser = (Users) FacesUtil.getSessionMapValue("LOGGEDIN_USER");
+        selectedCustomer.setUpdatedBy(loggedInUser.getUsername());
+        Customer customer=customerService.update(selectedCustomer);
+        if(customer!=null){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("SUCCESS : Customer "+customer.getName()+" Has Been Updated Successfully"));
+        }
     }
 
     public void delete() {
+        Users loggedInUser = (Users) FacesUtil.getSessionMapValue("LOGGEDIN_USER");
+        selectedCustomer.setUpdatedBy(loggedInUser.getUsername());
+        Customer customer=customerService.delete(selectedCustomer);
+        if(customer!=null){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("SUCCESS : Customer "+customer.getName()+" Has Been Deleted Successfully"));
+        }
+    }
+
+    public void showDialogue() {
+
+        RequestContext.getCurrentInstance().execute(
+                "PF('newCustomerDialog').show()");
+        return;
+
+    }
+
+    public void onRowSelect(SelectEvent event) {
+        setSelectedCustomer((Customer) event.getObject());
+        RequestContext.getCurrentInstance().execute("PF('customerDialog').show()");
+    }
+
+    public void reset(){
+        newCustomer=new Customer();
+        selectedCustomer=new Customer();
+    }
+
+    public void navigateToCreateCustomer(){
+
+        ConfigurableNavigationHandler nh = (ConfigurableNavigationHandler) FacesContext.getCurrentInstance().getApplication().getNavigationHandler();
+        nh.performNavigation("createcustomer?faces-redirect=true");
 
     }
 }
